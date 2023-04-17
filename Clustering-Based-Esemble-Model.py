851546e9d1582f6ada2b18a1539ce528694b0ced
myungbin <kki9621@naver.com>
2023-04-17 18:57:55 +0900

    Kmeans

---
+++ Clustering-Based-Esemble-Model.py
@@ -0,0 +1,68 @@
+import warnings
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+
+from src.features import utils, build_features_final, build_features
+from src.models import predict_model
+from src.train.train import train, evaluation, prediction_to_csv
+from src.data.make_dataset import DatasetLoader
+from src.config.config import seed_everything, cfg
+
+warnings.filterwarnings(action='ignore')
+seed_everything(cfg.SEED)
+
+scaler = MinMaxScaler()
+
+train_data = pd.read_csv(r'data\raw\train_data.csv')
+train_data = build_features_final.create_derived_features(train_data)
+
+test_data = pd.read_csv(r'data\raw\test_data.csv')
+test_data = build_features_final.create_derived_features(test_data)
+
+scaled_train_data = scaler.fit_transform(train_data)
+scaled_test_data = scaler.transform(test_data)
+
+scaled_train_data = pd.DataFrame(scaled_train_data, columns=train_data.columns)
+scaled_test_data = pd.DataFrame(scaled_test_data, columns=train_data.columns)
+
+scaled_train_data_df = scaled_train_data.copy()
+scaled_test_data_df = scaled_test_data.copy()
+
+grouped_train = scaled_train_data.groupby('type')
+
+drop_feature = ['type', 'motor_hp', 'air_end_temp', 'motor_rpm', 'motor_temp', 'motor_vibe', 'motor_current',
+                'air_inflow', "air_flow_pressure", "current_by_vibration", "airflow_per_rotation", "air_to_motor_ratio"]
+
+anomaly = []
+all_threshold = []
+for group_name, group_data in grouped_train:
+    test_group = scaled_test_data[scaled_test_data['type'] == group_name]
+    train_group = group_data.drop(drop_feature, axis=1).values
+    test_group = test_group.drop(drop_feature, axis=1).values
+
+    n_features = train_group.shape[1]
+    print(n_features)
+    dataloader = DatasetLoader(train_group, test_group)
+    train_loader, test_loader = dataloader.load
+    model = predict_model.PowerfulConv1DLSTMAutoencoder()
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+    train(train_loader, model, criterion, optimizer)
+
+    train_prediction, train_threshold = evaluation(train_loader, model)
+    test_prediction, test_threshold = evaluation(test_loader, model, min(train_threshold))
+
+    anomaly.append(test_prediction)
+    all_threshold.append(test_threshold)
+    print(f"finish {group_name}type")
+
+all_threshold = np.concatenate(all_threshold)
+anomaly = np.concatenate(anomaly)
+submission = prediction_to_csv(anomaly)
